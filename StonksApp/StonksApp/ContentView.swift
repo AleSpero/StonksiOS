0618552e1dee25134ed9b3fb43bba35a9c0f82e6
Alessandro Sperotti <alessandro.sperotti@cloudacademy.com>
2021-01-22 17:25:26 +0100

    first commit

---
+++ StonksApp/StonksApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StonksApp
+//
+//  Created by Alessandro Sperotti on 22/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StonksApp/StonksApp/StonksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StonksAppApp.swift
+//  StonksApp
+//
+//  Created by Alessandro Sperotti on 22/01/21.
+//
+
+import SwiftUI
+
+@main
+struct StonksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
